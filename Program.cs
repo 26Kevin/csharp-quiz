3c5fd7608d833cc48ea6c7f96441600c9d080084
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-06-12 14:48:27 -0600

    updating

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// QUIZ KEVIN
+Console.Clear();
+#nullable disable
+// Welcome
+Console.WriteLine("Welcome to the Marvel quiz");
+
+// Input
+bool loop1 = true;
+while(loop1) {
+    double counter1 = 0;
+    double percent1 = 0;
+    Console.WriteLine("\n1. What is spiderman  real full name?: ");
+    Console.Write("Q1 Answer:");
+    string answer1 = Console.ReadLine().ToLower();
+
+    // Input
+    if (answer1 == "peter parker") {
+        Console.WriteLine("Correct");
+        counter1 ++;
+        percent1 ++;
+    } else {
+        Console.WriteLine("Incorrect");
+    }
+}
+
+
